622e9b252133e3293b921372cfeb0e416012a285
William <william.chou@u-ark.com>
2023-03-25 17:20:39 +0800

    header init

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,42 @@
 <template>
-    <div class="bg-green">
-      <h1>Header</h1>
+  <div class="bg-green md:flex md:justify-center">
+    <div class="px-2 py-1 flex items-center justify-between md:max-w-[1200px]">
+      <div class="flex items-center">
+        <img
+          class="mr-3 md:hidden"
+          src="@/assets/images/hamburger.svg"
+          alt=""
+        />
+        <picture class="w-[180px]">
+          <source
+            srcset="@/assets/images/logo@2x.png"
+            media="(min-width: 991px)"
+          />
+          <img src="@/assets/images/logo.svg" />
+        </picture>
+      </div>
+
+      <div class="flex bg-white h-8">
+        <input type="text" />
+        <span class="text-green">
+          <img src="@/assets/images/search.svg" alt="" />
+          搜尋
+        </span>
+      </div>
+      <div class="flex items-center">
+        <img src="@/assets/images/search.svg" alt="" />
+        <img src="@/assets/images/user.svg" alt="" />
+        <img src="@/assets/images/cart.svg" alt="" />
+      </div>
     </div>
-  </template>
+  </div>
+</template>
 
-  <script>
-  export default {
-    // eslint-disable-next-line vue/multi-word-component-names
-    name: "Header",
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Header",
+};
+</script>
 
-  };
-  </script>
-
-  <style scoped lang="scss"></style>
+<style scoped lang="scss"></style>
