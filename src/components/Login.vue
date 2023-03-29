03d5a0ae503d2da97cee1aa1011dcc01e35e4452
William <william.chou@u-ark.com>
2023-03-29 17:20:17 +0800

    sm screen form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,33 +1,49 @@
 <template>
   <div class="md:flex justify-center">
-    <div class="md:flex w-full max-w-[1200px]">
+    <div class="flex justify-center md:justify-start w-full max-w-[1200px]">
       <div class="hidden md:block flex-shrink-0 mr-[-15%]">
         <img src="@/assets/images/pic-pharmacy.png" alt="" />
       </div>
       <form
-        class="px-[15px] pt-[30px] pb-[25px] bg-white md:flex-1  md:max-w-[620px] md:border border-silver md:rounded md:py-[60px] md:pr-28 md:pl-[50px]"
+        class="px-[15px] pt-[30px] pb-[25px] bg-white w-full sm:w-[70%] md:flex-1 md:max-w-[620px] sm:border border-silver sm:rounded md:py-[60px] md:px-[50px]"
         action=""
       >
-        <h3 class="text-xl mb-5 font-bold ">會員登入</h3>
+        <h3 class="text-xl mb-5 font-bold">會員登入</h3>
 
-        <div class="flex flex-col my-2 gap-y-2 md:flex-row md:justify-end">
-          <label class="text-left md:mr-4 md:text-right md:w-[60px]" for="">手機</label>
-          <input class="border rounded h-10 flex-1" type="text" />
+
+        <div
+          class="flex flex-col my-4 gap-y-2 md:flex-row md:justify-end md:mr-[70px]"
+        >
+          <label class="text-left md:mr-4 md:text-right md:w-[60px]" for=""
+            >手機</label
+          >
+          <input class="border rounded h-10 md:flex-1" type="text" />
         </div>
-        <div class="flex flex-col my-2 gap-y-2 md:flex-row md:justify-end">
-          <label class="text-left md:mr-4 md:text-right md:w-[60px]" for="">密碼</label>
-          <input class="border rounded h-10 flex-1" type="text" />
+        <div
+          class="flex flex-col my-4 gap-y-2 md:flex-row md:justify-end md:mr-[70px]"
+        >
+          <label class="text-left md:mr-4 md:text-right md:w-[60px]" for=""
+            >密碼</label
+          >
+          <input class="border rounded h-10 md:flex-1" type="text" />
         </div>
-        <div class="flex flex-col my-2 gap-y-2 md:flex-row md:justify-end">
-          <label class="text-left md:mr-4 md:text-right md:w-[60px]" for="">驗證碼</label>
-          <input class="border rounded h-10 flex-1" type="text" />
+        <div
+          class="flex flex-col my-4 gap-y-2 md:flex-row md:justify-end md:mr-[70px]"
+        >
+          <label class="text-left md:mr-4 md:text-right md:w-[60px]" for=""
+            >驗證碼</label
+          >
+          <input class="border rounded h-10 md:flex-1" type="text" />
         </div>
-        <div class="flex">
-          <img src="@/assets/images/captcha.png" alt="" />
-          <img src="@/assets/images/btn-refresh.svg" alt="" />
+        <div class="flex flex-col my-4 gap-y-2 md:flex-row md:mr-[70px]">
+          <span class="md:mr-4 md:w-[60px]"></span>
+          <div class="flex gap-3">
+            <img src="@/assets/images/captcha.png" alt="" />
+            <img src="@/assets/images/btn-refresh.svg" alt="" />
+          </div>
         </div>
         <button
-          class="bg-green text-white h-10 rounded w-full mt-[30px] mb-[15px]"
+          class="block bg-green text-white h-10 rounded w-full mt-[30px] mb-[15px] md:w-[calc(100%-140px)] mx-[70px]"
         >
           登入
         </button>
@@ -55,4 +71,6 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+</style>
