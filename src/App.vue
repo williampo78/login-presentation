9c606812f51f2a821f97c565e475cfd394df2a86
William <williampo78@gmail.com>
2023-03-29 22:40:22 +0800

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="app">
-    <Header />
-    <Nav />
-    <div class="min-h-[60vh] my-[40px]">
-      <router-view />
+  <div class="min-h-[100vh] flex flex-col justify-between" id="app">
+    <div>
+      <Header />
+      <Nav />
+      <div class="min-h-[60vh] my-[40px]">
+        <router-view />
+      </div>
     </div>
     <Footer />
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue"
-import Footer from "@/components/Footer.vue"
-import Nav from "@/components/Nav.vue"
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Nav from "@/components/Nav.vue";
 
 export default {
   components: {
@@ -20,7 +22,7 @@ export default {
     Footer,
     Nav,
   },
-}
+};
 </script>
 
 <style lang="scss">
